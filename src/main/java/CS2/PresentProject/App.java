8b8c840410e2748dded0343adee5dee075056e6a
Avinash <salveruavinash22@gmail.com>
2023-12-15 10:50:16 +0530

    Demo for Team

---
+++ src/main/java/CS2/PresentProject/App.java
@@ -0,0 +1,13 @@
+package CS2.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
